92c8799ebcc257bcf147516c959c80ba1c978ab7
ste-cartu <ste.cartu@gmail.com>
2024-07-18 14:48:05 +0200

    Ex 03 - COMPLETE

--- Exercise_03/03.1/main.cpp
+++ Exercise_03/03.1/main.cpp
@@ -29,20 +29,24 @@ int main(){
     /*––––––––––––––––––––––––––––––– DIRECT SAMPLING –––––––––––––––––––––––––––––––*/
 
     // call option
+    fmt::print("Call option: direct sampling:\n");
     double (*call_profit)(double, double) = Call_Profit;
     BA_Option call(extr, blocks, call_profit, S0, K, r, sigma);
     std::ofstream out_call("direct_call.csv");
     out_call << "blocks,extractions,profit,error" << std::endl;
     call.Progressive(out_call);
     out_call.close();
+    fmt::print("\n\n");
 
     // put option
+    fmt::print("Put option: direct sampling:\n");
     double (*put_profit)(double, double) = Put_Profit;
     BA_Option put(extr, blocks, put_profit, S0, K, r, sigma);
     std::ofstream out_put("direct_put.csv");
     out_put << "blocks,extractions,profit,error" << std::endl;
     put.Progressive(out_put);
     out_put.close();
+    fmt::print("\n\n");
 
 
     /*––––––––––––––––––––––––––––––– INDIRECT SAMPLING –––––––––––––––––––––––––––––––*/
@@ -51,22 +55,23 @@ int main(){
     unsigned int nstep = 100;
     
     // call option
+    fmt::print("Call option: indirect sampling:\n");
     call.SetSampling(T0, T, nstep);
     out_call.open("indirect_call.csv");
     out_call << "blocks,extractions,profit,error" << std::endl;
     call.Progressive(out_call);
     out_call.close();
+    fmt::print("\n\n");
 
     // put option
+    fmt::print("Put option: indirect sampling:\n");
     put.SetSampling(T0, T, nstep);
     out_put.open("indirect_put.csv");
     out_put << "blocks,extractions,profit,error" << std::endl;
     put.Progressive(out_put);
     out_put.close();
+    fmt::print("\n\n");
 
 
-
-
-    fmt::print("\n");
     return 0;
 }
